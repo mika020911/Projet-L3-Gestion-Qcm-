20639fa62a02487f3918866a34c472475b97f4f9
Eddys Mike <mikaeddys@gmail.com>
2026-05-24 20:29:06 +0300

    modif de database.java pour render

--- src/main/java/utils/connectionDB.java
+++ src/main/java/utils/connectionDB.java
@@ -7,17 +7,19 @@ public class connectionDB {
 
     public static Connection getConnection() throws Exception {
 
+        String url = System.getenv("DB_URL");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
 
-        String host     = System.getenv("MYSQLHOST")     != null ? System.getenv("MYSQLHOST")     : "localhost";
-        String port     = System.getenv("MYSQLPORT")     != null ? System.getenv("MYSQLPORT")     : "3306";
-        String database = System.getenv("MYSQLDATABASE") != null ? System.getenv("MYSQLDATABASE") : "gestion_qcm";
-        String user     = System.getenv("MYSQLUSER")     != null ? System.getenv("MYSQLUSER")     : "root";
-        String password = System.getenv("MYSQLPASSWORD") != null ? System.getenv("MYSQLPASSWORD") : "1234";
-
-        String url = "jdbc:mysql://" + host + ":" + port + "/" + database
-                   + "?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+        // MODE LOCAL si variables absentes
+        if (url == null) {
+            url = "jdbc:mysql://localhost:3306/gestion_qcm?useSSL=false";
+            user = "root";
+            password = "1234";
+        }
 
         Class.forName("com.mysql.cj.jdbc.Driver");
+
         return DriverManager.getConnection(url, user, password);
     }
 }
